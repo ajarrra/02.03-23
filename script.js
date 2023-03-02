05b3c16cf8994d6b90183555522ac67b8f52ad70
Koshoeva Ajar <ajarka1334@gmail.com>
2023-03-02 14:46:16 +0100

    finished second task

--- script.js
+++ script.js
@@ -10,5 +10,13 @@ for (i = 0; i < exampleList.length; i++) {
 
 //вывести в виде списка имя тире возраст
 const exampleList2 = [
-    { name : "Bakyt" , age: 18}
-] 
+    { name: "bakyt", age: 18 },
+    { name: "dosbol", age: 32 },
+];
+const ul1 = document.createElement("ul");
+document.body.append(ul1);
+for (i = 0; i < exampleList2.length; i++) {
+    const list1 = document.createElement("li");
+    ul1.append(list1);
+    list1.textContent = exampleList2[i].name + " - " + exampleList2[i].age;
+}
